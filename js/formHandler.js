d6496a6f368b06931d6d96c39357ad65d37a25ce
lolulo69 <lefortlucas1@gmail.com>
2022-11-07 13:40:11 +0100

    Upload final du site après optimisation

---
+++ js/formHandler.js
@@ -0,0 +1,56 @@
+$(function()
+{	
+	$("input,textarea").jqBootstrapValidation(
+    {
+     	preventSubmit: true,
+     	submitSuccess: function($form, event)
+	 	{			
+			if(!$form.attr('action')) // Check form doesnt have action attribute
+			{
+				event.preventDefault(); // prevent default submit behaviour
+			
+				var processorFile = getProcessorPath($form);
+				var formData = {};
+
+				$form.find("input, textarea, option:selected").each(function(e) // Loop over form objects build data object
+				{		
+					var fieldData =  $(this).val();
+					var fieldID =  $(this).attr('id');
+				
+					if($(this).is(':checkbox')) // Handle Checkboxes
+					{
+						fieldData = $(this).is(":checked");
+					}
+					else if($(this).is(':radio')) // Handle Radios
+					{
+						fieldData = $(this).val()+' = '+$(this).is(":checked");
+					}
+					else if($(this).is('option:selected')) // Handle Option Selects
+					{
+						fieldID = $(this).parent().attr('id');
+					}
+					
+					formData[fieldID] = fieldData;		
+				});
+	
+			}
+         },
+         filter: function() // Handle hidden form elements
+		 {
+			 return $(this).is(":visible");
+         },
+	 });
+	 
+	 // Get Path to processor PHP file
+	 function getProcessorPath(form)
+	 {
+		var path = "./includes/"+form.attr('id')+".php";
+		
+		if(form.attr('template-path')) // Check For Template path
+		{
+			path = form.attr('template-path')+"/includes/"+form.attr('id')+".php";
+		}
+		
+	 	return path
+	 }
+});
